56d54f7f25bd945134d3626a8eb9b12518b50a7f
Jo <joannadawnmiller@gmail.com>
2021-01-15 13:43:09 -0500

    troublshoot how to calculate 3 options with 5 questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
   $("#quiz").submit(function(event) {
-    const colorAnswer = parseInt($("#color").val());
-     event.preventDefault();
-   
-  if (colorAnswer === 1) {
-    console.log("yes");
-  }
+    const colorAnswer = $("#color").val();
+    const seasonAnswer = $("#season").val();
+    const foodAnswer = $("#food").val();
+    const regionAnswer = $("#region").val();
+    const musicAnswer = $("#music").val();
+    event.preventDefault();
+    
+    if (colorAnswer === 1) {
+      $("#answer").show();
+  
   }); 
 });
 
