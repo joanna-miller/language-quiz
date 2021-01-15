2388f84e5986c58a1d8f1e497ed40a55b557ba8a
Jo <joannadawnmiller@gmail.com>
2021-01-15 14:56:33 -0500

    finish validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+  let forms = document.getElementsByClassName('needs-validation');
+  var validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+  
+
   $("#quiz").submit(function(event) {
     const colorAnswer = parseInt($("#color").val());
     const seasonAnswer = parseInt($("#season").val());
@@ -6,15 +18,15 @@ $(document).ready(function () {
     const regionAnswer = parseInt($("#region").val());
     const musicAnswer = parseInt($("#music").val());
     
-    if (colorAnswer + seasonAnswer + foodAnswer + regionAnswer + musicAnswer <= 5) {
+    if (colorAnswer + seasonAnswer + foodAnswer + regionAnswer + musicAnswer >= 5 && colorAnswer + seasonAnswer + foodAnswer + regionAnswer + musicAnswer <= 7) {
       $("#answer").show();
       $("#c, #python").hide();
-    } else if (colorAnswer + seasonAnswer + foodAnswer + regionAnswer + musicAnswer >= 15) {
+      } else if (colorAnswer + seasonAnswer + foodAnswer + regionAnswer + musicAnswer > 7 && colorAnswer + seasonAnswer + foodAnswer + regionAnswer + musicAnswer < 13) {
+      $("#answer").show();
+      $("#ruby, #c").hide(); 
+    } else if (colorAnswer + seasonAnswer + foodAnswer + regionAnswer + musicAnswer >= 13) {
       $("#answer").show();
       $("#ruby, #python").hide();
-    } else {
-      $("#answer").show();
-      $("#ruby, #c").hide();
     }
     event.preventDefault();
   }); 
